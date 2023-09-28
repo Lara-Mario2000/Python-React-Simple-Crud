a4b324e395f4fb9212075eb20a712388f44d531d
Mario <85653665+Lara-Mario2000@users.noreply.github.com>
2023-09-28 16:35:27 -0600

    Update config.py

--- API_Flask_POSTGRESQL/config.py
+++ API_Flask_POSTGRESQL/config.py
@@ -1,5 +1,5 @@
 class Config:
-    DATABASE_URL = 'postgres://mario:hfsjikArkUFr9bG0yTfLtJtAMByT1ogN@dpg-ck902hk7m7is73bse410-a.oregon-postgres.render.com/simple_crud_i2wl'
+    DATABASE_URL = 'DATABASE_URL'
 
 class DevelopmentConfig():
     DEBUG = True
